384ac0c691167aa9cfaa6405c1abba4be03b9cb8
Andile Bongumenzi Dumakude <andilebshange@gmail.com>
2020-06-01 13:57:08 +0200

    Add project files.

---
+++ CattlewatchAppFinal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CattlewatchAppFinal.MvcApplication" Language="C#" %>
---
+++ CattlewatchAppFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CattlewatchAppFinal.Startup))]
+namespace CattlewatchAppFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
