c55d2781b596d9de288b4fa6a686f8f19a7d7246
Andile <andilebshange@gmail.com>
2020-06-05 14:18:00 +0200

    email

--- CattlewatchAppFinal/Controllers/HomeController.cs
+++ CattlewatchAppFinal/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CattlewatchAppFinal.Models;
+using System.Net.Mail;
+using System.Text;
 
 namespace CattlewatchAppFinal.Controllers
 {
@@ -20,11 +23,46 @@ namespace CattlewatchAppFinal.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Contact(Contact contact)
         {
-            ViewBag.Message = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    StringBuilder message = new StringBuilder();
+                    MailAddress from = new MailAddress(contact.Email.ToString());
+                    message.Append("Name: " + contact.FirstName + "\n");
+                    message.Append("Email: " + contact.Email + "\n");
+                    message.Append("Telephone: " + contact.Telephone + "\n\n");
+                    message.Append("Subject: " + contact.Supject + "\n\n");
+                    message.Append(contact.Message);
+                   
+                    MailMessage msg = new MailMessage();
+                    msg.From = from;
+                    msg.To.Add("andilebshange@gmail.com");
+                    msg.Subject = "Query on Website";
+                    msg.Body = message.ToString();
 
-            return View();
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new System.Net.NetworkCredential("cattlewatch01@gmail.com", "WatchCattle-2011");
+                    smtp.Send(msg);
+
+                    
+                    ViewBag.Message = "Thank you for contacting us";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View("Index");
         }
     }
 }
---
+++ CattlewatchAppFinal/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CattlewatchAppFinal.Models
+{
+    public class Contact
+    {
+          
+            public string FirstName { get; set; }
+            public string Supject { get; set; }
+            [Required]
+            public string Email { get; set; }
+            [Required]
+            public string Message { get; set; }
+            [Required]
+            public int Telephone { get; set; }
+
+
+    }
+}
