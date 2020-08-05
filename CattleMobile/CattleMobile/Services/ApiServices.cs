6c7e4251db9521e10bad54dd2d5f48cc19530103
Andile <andilebshange@gmail.com>
2020-08-05 15:23:43 +0200

    update

---
+++ CattleMobile/CattleMobile/Services/ApiServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CattleMobile.Services
+{
+    public class ApiServices
+    {
+        public async Task RegisterAsync(string email, string password, string confirmPassword)
+        {
+            var clinet = new HttpClient();
+        }
+    }
+}
---
+++ CattleMobile/CattleMobile/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CattleMobile.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CattleMobile.ViewModel
+{
+    public class RegisterViewModel
+    {
+        ApiServices _apiServices = new ApiServices();
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new Command(() =>
+               {
+                   _apiServices.RegisterAsync(Email, Password, ConfirmPassword);
+               });
+            }
+        }
+    }
+}
